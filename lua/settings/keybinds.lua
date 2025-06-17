5265ccf7ab52ef38021b855766e90cdc2aea872a
Joan Lucas <jlmsc314@gmail.com>
2025-06-17 13:11:12 -0300

    Removed unused keybind, adjusted comments

--- lua/settings/keybinds.lua
+++ lua/settings/keybinds.lua
@@ -1,21 +1,19 @@
--- NOTE: All keybinds must be defined here, so it will
+-- All keybinds must be defined here, so it will
 --  be easier to locate or change them later.
 
 -- NOTE: These are used used by 'lua/lsp/completion.lua'
 
--- Selects the next completion.
+-- Selects next completion.
 vim.keymap.set("i", "<Tab>", function()
   return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
 end, { expr = true, noremap = true })
--- Selects the previous completion.
+
+-- Selects previous completion.
 vim.keymap.set("i", "<S-Tab>", function()
   return vim.fn.pumvisible() == 1 and "<C-p>" or "<Tab>"
 end, { expr = true, noremap = true })
--- Hides the completion menu but keeps on Insert mode.
+
+-- Hides the completion menu but stay in Insert mode.
 vim.keymap.set("i", "<Esc>", function()
   return vim.fn.pumvisible() == 1 and "<C-e>" or "<Esc>"
 end, { expr = true, noremap = true })
--- Triggers the completion menu.
-vim.keymap.set("i", "<C-Space>", function()
-  vim.lsp.completion.get()
-end, { noremap = true, silent = true })
